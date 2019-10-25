52862b10e808c5a15267671b9505de5cf10ed06e
MinexAutomation <minex.automation@gmail.com>
2019-10-24 18:10:21 -0700

    Added deploy source and destination file-system site provider interfaces.

---
+++ source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentDestinationFileSystemSiteProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Gepidia;
+
+
+namespace R5T.Antium
+{
+    public interface IDeploymentDestinationFileSystemSiteProvider
+    {
+        FileSystemSite GetDeploymentDestinationFileSystemSite();
+    }
+}
---
+++ source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentSourceFileSystemSiteProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Gepidia;
+
+
+namespace R5T.Antium
+{
+    public interface IDeploymentSourceFileSystemSiteProvider
+    {
+        FileSystemSite GetDeploymentSourceFileSystemSite();
+    }
+}
