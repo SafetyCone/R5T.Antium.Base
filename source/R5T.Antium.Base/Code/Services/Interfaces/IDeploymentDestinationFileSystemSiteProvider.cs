915d7674ae324576ea9af2e345f6ceef4f5e8b8d
MinexAutomation <minex.automation@gmail.com>
2020-02-18 23:40:06 -0800

    Added documentation.

--- source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentDestinationFileSystemSiteProvider.cs
+++ source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentDestinationFileSystemSiteProvider.cs
@@ -5,6 +5,9 @@ using R5T.Gepidia;
 
 namespace R5T.Antium
 {
+    /// <summary>
+    /// Provides the deployment destination file system site.
+    /// </summary>
     public interface IDeploymentDestinationFileSystemSiteProvider
     {
         FileSystemSite GetDeploymentDestinationFileSystemSite();
--- source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentSourceFileSystemSiteProvider.cs
+++ source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentSourceFileSystemSiteProvider.cs
@@ -5,6 +5,9 @@ using R5T.Gepidia;
 
 namespace R5T.Antium
 {
+    /// <summary>
+    /// Provides the deployment source file system site.
+    /// </summary>
     public interface IDeploymentSourceFileSystemSiteProvider
     {
         FileSystemSite GetDeploymentSourceFileSystemSite();
