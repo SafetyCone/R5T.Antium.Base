bd1a8b4f13ed1f9af5cabd39f4a3ae2923dde753
MinexAutomation <minex.automation@gmail.com>
2020-02-23 16:46:50 -0800

    Added deployment source and destination secrets directory file system site provider service definitions.

---
+++ source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Gepidia;
+
+
+namespace R5T.Antium
+{
+    public interface IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider
+    {
+        FileSystemSite GetDeploymentDestinationSecretsDirectoryFileSystemSite();
+    }
+}
---
+++ source/R5T.Antium.Base/Code/Services/Interfaces/IDeploymentSource_SecretsDirectory_FileSystemSiteProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Gepidia;
+
+
+namespace R5T.Antium
+{
+    public interface IDeploymentSource_SecretsDirectory_FileSystemSiteProvider
+    {
+        FileSystemSite GetDeploymentSourceSecretsDirectoryFileSystemSite();
+    }
+}
